d683b1ea654c0657b12adb35bf00a9d4a5816f00
Marcelo Massao Kataoka Higaskino <massaohigaskino@gmail.com>
2017-06-07 21:30:51 -0300

    Incorrect partial test implementation

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -16,6 +16,23 @@
 
 package com.example.android.teatime;
 
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
  * corresponding OrderActivity.
@@ -27,15 +44,23 @@ package com.example.android.teatime;
 
 
 // TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class MenuActivityScreenTest {
 
     // TODO (2) Add the rule that provides functional testing of a single activity
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityTestRule =
+            new ActivityTestRule<MenuActivity>(MenuActivity.class);
 
     // TODO (3) Finish writing this test which will click on a gridView Tea item and verify that
     // the OrderActivity opens up with the correct tea name displayed.
-
+    @Test
     public void clickGridViewItem_OpensOrderActivity() {
-
+        DataInteraction dataInteraction = onData(CoreMatchers.anything());
+        dataInteraction.inAdapterView(withId(R.id.tea_grid_view)).onChildView(withChild(withText(R.string.green_tea_name)))
+                .perform(click());
+        onView(withId(R.id.tea_name_text_view))
+                .check(ViewAssertions.matches(withText(R.string.green_tea_name)));
     }
 
 }
